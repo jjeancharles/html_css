d5d630c217933be670b13a05190e947b5da90dd1
jjeancharles <jjeancharles@wesleyan.edu>
2022-02-16 15:35:37 -0500

    notes

--- php_tutorial.php
+++ php_tutorial.php
@@ -5,3 +5,9 @@
   Hello <?php echo $user; ?>
   </body>
   </html>
+
+  <!--
+    Reason why this code doesn't work is because we do not have the php
+    interpreter, and also we don't have a database to collect the data
+  -->
+
